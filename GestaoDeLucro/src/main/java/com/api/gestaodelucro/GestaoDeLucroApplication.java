7ffc6e82d598049cef8db2f697e0d11c8ed406a6
ViniciusOBrito <peixonauta.vb@gmail.com>
2024-08-21 18:18:56 -0300

    init commit

---
+++ GestaoDeLucro/src/main/java/com/api/gestaodelucro/GestaoDeLucroApplication.java
@@ -0,0 +1,13 @@
+package com.api.gestaodelucro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoDeLucroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestaoDeLucroApplication.class, args);
+    }
+
+}
---
+++ GestaoDeLucro/src/test/java/com/api/gestaodelucro/GestaoDeLucroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.gestaodelucro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDeLucroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
